dea2679c3136cfa17a08d6077e14c212f1a56474
laila <laila@fake.com>
2018-08-24 00:04:02 +0300

    use prepared statement

--- src/main/java/Query.java
+++ src/main/java/Query.java
@@ -1,28 +1,24 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 
 public class Query {
     public void selectAll(){
-        String sql ="SELECT * FROM Author";
-        //"SELECT Author.First_Name,Title from Author left join Book ON Author.Author_id=Book.Author_id";
-        //First_Name,Last_Name,author_id,Year_of_Birth
-
-        // establishing connection
-        try (Connection conn = SQLconnection.connect();
-             //Statement defines the methods tht enable you to send SQL commands and receive data from db
-             Statement stmt  = conn.createStatement();
-             //executeQuery() returns the object of ResultSet that is used to get all the records of a table
-             ResultSet rs = stmt.executeQuery(sql)){
-
-            // loop through the result set
-            while (rs.next()) {
-                System.out.println(rs.getString("First_Name") +  "\t" +
-                        rs.getString("Last_Name") + "\t" +
-                        rs.getString("Year_of_Birth") + "\t" +
-                        rs.getInt("author_id"));
+        PreparedStatement updateFirst_Name = null;
+        PreparedStatement updateYear_of_Birth = null;
+        String updateQueryFirst_Name =
+                "update Author set First_Name = ? where Author_id = ?";
+        try (Connection conn = SQLconnection.connect()){
+             {
+                 try {
+                     conn.setAutoCommit(false);
+                     updateFirst_Name = conn.prepareStatement(updateQueryFirst_Name);
+                     updateFirst_Name.setString(1, "Test");
+                     updateFirst_Name.setInt(2, 1);
+                     updateFirst_Name.executeUpdate();
+                     conn.commit();
+                 } catch (SQLException e1) {
+                     e1.printStackTrace();
+                 }
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
